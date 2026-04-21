3125a603882711a8434929c4ed95e314f364001d
jujullsjuice <markjulliusdelossantos@gmail.com>
2026-04-22 02:46:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const notes = [
-"You don’t have to be everything all the time.",
-"I’m still here with you.",
+"I'm with you through the good times, and definitely through the heavy ones.",
+"I admire that you protect your energy when you need space.",
+"I love how we can go from serious to chaotic in minutes.",
+"You’re not difficult to be with.",
+"I admire how much you care.",
+"I love how smart you are.",
+"I hope you are taking care of yourself.",
+"I hope you are eating well.",
 "You’re not a burden.",
 "I appreciate you so much.",
 "We’ll figure things out together.",
 "You’re allowed to rest.",
 "I like being with you.",
+"You do not disappoint me.",
 "You matter to me."
 ];
 
