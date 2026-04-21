ac95dc722ba87cd7bfc37becd692a50b56386b1d
jujullsjuice <markjulliusdelossantos@gmail.com>
2026-04-21 23:40:11 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const notes = [
+
+"You don’t have to carry everything alone. I’m still here.",
+
+"Even when things feel confusing between us, I’m not going anywhere.",
+
+"If we argue again someday, it doesn’t mean we’re breaking. It means we’re still learning each other.",
+
+"You’re not a burden to me. Never were.",
+
+"I like seeing you happy. It makes my day softer."
+
+];
+
+function showNote(index) {
+
+document.getElementById("noteBox").innerText = notes[index];
+
+}
